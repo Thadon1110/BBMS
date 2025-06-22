ca563f36bf24ae35a43e8d55bf32dd01ab96dc05
Thadon1110 <damian.duda2302@gmail.com>
2025-06-22 11:27:11 +0200

    speakers changed

--- src/components/Speakers/index.js
+++ src/components/Speakers/index.js
@@ -1,11 +1,22 @@
 import styles from './Speakers.module.scss';
 import SectionHeader from '../SectionHeader';
 import Image from 'next/image';
-import { FaLinkedinIn, FaWikipediaW, FaGlobe } from 'react-icons/fa6';
+import { FaLinkedinIn, FaWikipediaW, FaGlobe, FaInstagram } from 'react-icons/fa6';
 
 const speakersData = [
 	{
 		id: 1,
+		name: 'Joanna Idzikowska',
+		role: 'Właścicielka i Dyrektor Zarządzająca Trawialnia.eu Sp. z o.o.',
+		description:
+			'Energetyczna liderka chemicznej obróbki metali – prekursorka trawienia i pasywacji stali nierdzewnej w Polsce. Założyła Trawialnia.eu (dawniej Chimimeca Polska) z francuskim kapitałem, kieruje firmą od ponad 20 lat, zdobywając m.in. tytuł w Diamentach Forbesa 2025. Fundatorka Fundacji Serca Kreacja i autorka “Mocna jak stal”, która łączy duchowość i biznes.',
+		image: '/speakers/joanna_idzikowska.jpg',
+		linkedin: 'https://www.linkedin.com/in/joanna-idzikowska/',
+		instagram: 'https://www.instagram.com/joanna.idzikowska/',
+		page: 'https://joannaidzikowska.pl/',
+	},
+	{
+		id: 2,
 		name: 'dr hab. inż. Zbigniew Brytan',
 		role: 'Profesor uczelni na Politechnice Śląskiej',
 		description:
@@ -14,54 +25,90 @@ const speakersData = [
 		linkedin: 'https://www.linkedin.com/in/zbigniew-brytan-0b09b416/',
 	},
 	{
-		id: 2,
-		name: 'prof. dr hab. inż. Juliusz Orlikowski',
-		role: 'Profesor na Politechnice Gdańskiej',
-		description:
-			'Ekspert w dziedzinie mechaniki materiałów i symulacji komputerowych struktur inżynierskich. Prowadzi zespoły badawcze specjalizujące się w modelowaniu numerycznym oraz technologii projektowania wspomaganego komputerowo (CAD), a także mentor młodych naukowców akademickich.',
-		image: '/speakers/juliusz_orlikowski.jpg',
-		linkedin: 'https://www.linkedin.com/in/juliusz-orlikowski-23173b7/',
-		wikipedia: 'https://pl.wikipedia.org/wiki/Juliusz_Orlikowski',
-	},
-	{
 		id: 3,
-		name: 'dr hab. inż. Stefan Krakowiak',
-		role: 'Profesor uczelni na Politechnice Gdańskiej',
+		name: 'mgr inż. Beata Drzazga',
+		role: 'Prezes BetaMed S.A., członkini Rady Społecznej Politechniki Śląskiej',
 		description:
-			'Specjalista w dziedzinie elektrotechniki i systemów energetycznych. Zajmuje się technologiami ochrony przed korozją, nowoczesnymi powłokami ochronnymi oraz efektywnością energetyczną. Autor licznych publikacji naukowych oraz aktywny uczestnik projektów badawczych realizowanych na Wydziale Chemicznym PG.',
-		image: '/speakers/stefan_krakowiak.jpg',
-		linkedin: 'https://www.linkedin.com/in/stefan-krakowiak-4776a3177/',
+			'Przedsiębiorczyni z wizją i empatią – założycielka i prezes BetaMed S.A., jednej z największych firm medycznych w Polsce (3200+ pracowników, 91 placówek). Członkini Rady Społecznej PŚ. Mentor, filantrop i aktywna prelegentka promująca etyczny biznes i ESG.',
+		image: '/speakers/beata_drzazga.jpg',
+		linkedin: 'https://www.linkedin.com/in/zbigniew-brytan-0b09b416/',
 	},
 	{
 		id: 4,
-		name: 'Andrzej Michalski-Stępkowski',
-		role: 'Prezes Stowarzyszenia Stal Nierdzewna & Polskiego Stowarzyszenia Aluminium',
+		name: 'Ireneusz Popiół',
+		role: 'Prezes Zarządu CHEMET S.A.',
 		description:
-			'Od 2016 r. pełni rolę prezesa Stowarzyszenia Stal Nierdzewna (SSN), reprezentującego czołowe firmy z branży w Polsce. Od 2021 r. kieruje również Polskim Stowarzyszeniem Aluminium. Wcześniej związany jako dziennikarz i redaktor w „Rzeczpospolitej”, założyciel magazynu Nowa Stal, ekspert branży stalowej i aluminiowej z ponad 15-letnim doświadczeniem.',
-		image: '/speakers/andrzej_michalski_stepkowski.jpg',
-		linkedin: 'https://www.linkedin.com/in/andrzejmichalskist%C4%99pkowski/',
+			'Łączy naukę z biznesem – wprowadził symulacje numeryczne i własne laboratorium w CHEMET. Odznaczony Krzyżem Zasługi za wkład w rozwój przemysłu naftowo‑gazowego.',
+		image: '/speakers/ireneusz_popiol.jpg',
+		linkedin: 'https://www.linkedin.com/in/zbigniew-brytan-0b09b416/',
 	},
 	{
 		id: 5,
-		name: 'Green Reporting',
-		role: 'Startup technologiczny',
+		name: 'dr inż. Jerzy Niagaj, prof. nzw.',
+		role: 'Profesor nadzwyczajny, Łukasiewicz – GIT (Centrum Spawalnictwa)',
 		description:
-			'Green Reporting to polska firma założona w 2023 roku, specjalizująca się w automatyzacji procesów CBAM – Carbon Border Adjustment Mechanism. Oferuje narzędzie „Reporting Assistant”, które wspiera importery i służby celne w zbieraniu danych, generowaniu raportów CO₂ i zapewnianiu zgodności z regulacjami UE-wszystko z wykorzystaniem AI, ML i analizy danych.',
-		image: '/speakers/greenreporting.jpg',
-		linkedin: 'https://www.linkedin.com/company/green-reporting-ltd/',
-		page: 'https://greenreporting.eu/',
+			'Ekspert od energetyki jądrowej i technologii spawalniczych (MMA/TIG/MIG/FCAW/SAW), z 35-letnim doświadczeniem. Członek Kapituły MKES 2025, autor wytycznych ASME/AFCEN, licencjonowany Inżynier Spawalnik IWE, Visiting Professor w Lwowie 2021.',
+		image: '/speakers/jerzy_niagaj.jpg',
+		linkedin: 'https://www.linkedin.com/in/zbigniew-brytan-0b09b416/',
 	},
 	{
 		id: 6,
-		name: 'Aperam',
-		role: 'Globalna firma ze stali nierdzewnej',
+		name: 'Małgorzata Różyńska-Woźniak',
+		role: 'Prezes Zarządu PROMONTIN PROJECT Sp. z o.o.',
 		description:
-			'Aperam to światowy lider w produkcji stali nierdzewnej, elektrycznej i specjalistycznej, silnie zorientowany na recykling i zrównoważony rozwój. Posiada zdolność produkcyjną 2,5 mln ton w Europie i Brazylii, w ponad 40 krajach, przy zaangażowaniu ~12 000 pracowników. W 90% wykorzystuje złom, a rozszerza ofertę o ogniwa recyklingowe, bio-charcoal czy rozwój cirkularnej ekonomii.',
-		image: '/speakers/aperam.jpg',
-		linkedin: 'https://www.linkedin.com/company/aperam-poland/',
-		page: 'https://www.aperam.com/pl/',
-		wikipedia: 'https://en.wikipedia.org/wiki/Aperam',
+			'Zarządza PROMONTIN PROJECT – firmą inżynieryjno‑montażową specjalizującą się w EPC, instalacjach, filtracji i stalowych konstrukcjach. Firma uruchomiła nową halę w strefie Dębska Wola i realizuje projekty dekarbonizacyjne.',
+		image: '/speakers/małgorzata_rozynska_wozniak.jpg',
+		linkedin: 'https://www.linkedin.com/in/zbigniew-brytan-0b09b416/',
 	},
+	// {
+	// 	id: 2,
+	// 	name: 'prof. dr hab. inż. Juliusz Orlikowski',
+	// 	role: 'Profesor na Politechnice Gdańskiej',
+	// 	description:
+	// 		'Ekspert w dziedzinie mechaniki materiałów i symulacji komputerowych struktur inżynierskich. Prowadzi zespoły badawcze specjalizujące się w modelowaniu numerycznym oraz technologii projektowania wspomaganego komputerowo (CAD), a także mentor młodych naukowców akademickich.',
+	// 	image: '/speakers/juliusz_orlikowski.jpg',
+	// 	linkedin: 'https://www.linkedin.com/in/juliusz-orlikowski-23173b7/',
+	// 	wikipedia: 'https://pl.wikipedia.org/wiki/Juliusz_Orlikowski',
+	// },
+	// {
+	// 	id: 3,
+	// 	name: 'dr hab. inż. Stefan Krakowiak',
+	// 	role: 'Profesor uczelni na Politechnice Gdańskiej',
+	// 	description:
+	// 		'Specjalista w dziedzinie elektrotechniki i systemów energetycznych. Zajmuje się technologiami ochrony przed korozją, nowoczesnymi powłokami ochronnymi oraz efektywnością energetyczną. Autor licznych publikacji naukowych oraz aktywny uczestnik projektów badawczych realizowanych na Wydziale Chemicznym PG.',
+	// 	image: '/speakers/stefan_krakowiak.jpg',
+	// 	linkedin: 'https://www.linkedin.com/in/stefan-krakowiak-4776a3177/',
+	// },
+	// {
+	// 	id: 4,
+	// 	name: 'Andrzej Michalski-Stępkowski',
+	// 	role: 'Prezes Stowarzyszenia Stal Nierdzewna & Polskiego Stowarzyszenia Aluminium',
+	// 	description:
+	// 		'Od 2016 r. pełni rolę prezesa Stowarzyszenia Stal Nierdzewna (SSN), reprezentującego czołowe firmy z branży w Polsce. Od 2021 r. kieruje również Polskim Stowarzyszeniem Aluminium. Wcześniej związany jako dziennikarz i redaktor w „Rzeczpospolitej”, założyciel magazynu Nowa Stal, ekspert branży stalowej i aluminiowej z ponad 15-letnim doświadczeniem.',
+	// 	image: '/speakers/andrzej_michalski_stepkowski.jpg',
+	// 	linkedin: 'https://www.linkedin.com/in/andrzejmichalskist%C4%99pkowski/',
+	// },
+	// {
+	// 	id: 5,
+	// 	name: 'Green Reporting',
+	// 	role: 'Startup technologiczny',
+	// 	description:
+	// 		'Green Reporting to polska firma założona w 2023 roku, specjalizująca się w automatyzacji procesów CBAM – Carbon Border Adjustment Mechanism. Oferuje narzędzie „Reporting Assistant”, które wspiera importery i służby celne w zbieraniu danych, generowaniu raportów CO₂ i zapewnianiu zgodności z regulacjami UE-wszystko z wykorzystaniem AI, ML i analizy danych.',
+	// 	image: '/speakers/greenreporting.jpg',
+	// 	linkedin: 'https://www.linkedin.com/company/green-reporting-ltd/',
+	// 	page: 'https://greenreporting.eu/',
+	// },
+	// {
+	// 	id: 6,
+	// 	name: 'Aperam',
+	// 	role: 'Globalna firma ze stali nierdzewnej',
+	// 	description:
+	// 		'Aperam to światowy lider w produkcji stali nierdzewnej, elektrycznej i specjalistycznej, silnie zorientowany na recykling i zrównoważony rozwój. Posiada zdolność produkcyjną 2,5 mln ton w Europie i Brazylii, w ponad 40 krajach, przy zaangażowaniu ~12 000 pracowników. W 90% wykorzystuje złom, a rozszerza ofertę o ogniwa recyklingowe, bio-charcoal czy rozwój cirkularnej ekonomii.',
+	// 	image: '/speakers/aperam.jpg',
+	// 	linkedin: 'https://www.linkedin.com/company/aperam-poland/',
+	// 	page: 'https://www.aperam.com/pl/',
+	// 	wikipedia: 'https://en.wikipedia.org/wiki/Aperam',
+	// },
 ];
 
 export default function Speakers() {
@@ -100,6 +147,12 @@ export default function Speakers() {
 								{speaker.wikipedia && (
 									<a href={speaker.wikipedia} className={styles.linkedin__link} target='_blank' rel='noopener noreferrer'>
 										<FaWikipediaW />
+									</a>
+								)}
+
+								{speaker.instagram && (
+									<a href={speaker.instagram} className={styles.linkedin__link} target='_blank' rel='noopener noreferrer'>
+										<FaInstagram />
 									</a>
 								)}
 
